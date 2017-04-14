297b6a1f240b637eddd6766aacc418a856fc80e4
799616945@qq.com <799616945@qq.com>
2017-04-14 15:34:50 +0800

    first commit

---
+++ src/components/FoodDetail/FoodDetailComponent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<h1>sdsad</h1>
+	</div>
+</template>
+<script>
+	import http from '../../utils/HttpClient'
+	import VueRouter from '../../router/index'
+
+	export default {
+		name: 'home',
+		data: function(){
+			return {
+				msg:"kam"
+			}
+		}
+	}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import http from '../utils/HttpClient'
+
+// import HomeComponent from '../components/home/HomeComponent.vue'
+import LoginComponent from '../components/login/LoginComponent.vue'
+import FoodDetailComponent from '../components/FoodDetail/FoodDetailComponent.vue'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	routes: [{
+		path: '/login',
+		name: 'login',
+		component: LoginComponent
+	},{
+        path:'/FoodDetail',
+        name:'FoodDetail',
+        component:FoodDetailComponent
+    }]
+})
+
+//对将要进入的路由进行权判断
+// router.beforeEach((to, from, next) =>{
+// 	if(to.path != '/login'){
+// 		// http.get('./src/data/sesson.json')
+// 		// .then(response => {
+// 		// 	if(!response.login){
+// 		// 		router.replace('login');
+// 		// 	}
+// 		// 	next();
+// 		// })
+// 		http.get('http://localhost:888/getsession')
+// 		.then(response => {
+// 			if(!response.name){
+// 				router.replace('login');
+// 			}
+// 			next()
+// 		});
+// 	} else {
+// 		next();
+// 	}
+// })
+
+export default router
